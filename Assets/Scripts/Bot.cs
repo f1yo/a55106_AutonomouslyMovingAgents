4dbd428e08446027c3591bac637333189e830a21
fly <Benylaranjinha@gmail.com>
2025-11-22 16:33:37 +0000

    Seek and Flee

---
+++ Assets/Scripts/Bot.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Bot : MonoBehaviour
+{
+    NavMeshAgent agent;
+    [SerializeField] GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    void Seek(Vector3 location)
+    {
+        agent.SetDestination(location);
+    }
+
+    void Flee(Vector3 location)
+    {
+        Vector3 fleeVector = location - this.transform.position;
+        agent.SetDestination(this.transform.position - fleeVector);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Flee(target.transform.position);
+    }
+}
