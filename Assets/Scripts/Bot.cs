9be8c92dce07048a49fabd9ce466ca2a1113323c
fly <Benylaranjinha@gmail.com>
2025-11-22 17:43:07 +0000

    Hide

--- Assets/Scripts/Bot.cs
+++ Assets/Scripts/Bot.cs
@@ -80,9 +80,78 @@ public class Bot : MonoBehaviour
         Seek(targetWorld);
     }
 
+    void Hide()
+    {
+        float dist = Mathf.Infinity;
+        Vector3 chosenSpot = Vector3.zero;
+
+        for(int i = 0; i < World.Instance.GetHidingSpots().Length; i++)
+        {
+            Vector3 hideDir = World.Instance.GetHidingSpots()[i].transform.position - target.transform.position;
+            Vector3 hidePosition = World.Instance.GetHidingSpots()[i].transform.position + hideDir.normalized * 5;
+
+            if(Vector3.Distance(this.transform.position, hidePosition) < dist)
+            {
+                chosenSpot = hidePosition;
+                dist = Vector3.Distance(this.transform.position, hidePosition);
+            }
+        }
+
+        Seek(chosenSpot);
+    }
+
+    void CleverHide()
+    {
+        float dist = Mathf.Infinity;
+        Vector3 chosenSpot = Vector3.zero;
+        Vector3 chosenDir = Vector3.zero;
+        GameObject chosenGO = World.Instance.GetHidingSpots()[0];
+
+        for (int i = 0; i < World.Instance.GetHidingSpots().Length; i++)
+        {
+            Vector3 hideDir = World.Instance.GetHidingSpots()[i].transform.position - target.transform.position;
+            Vector3 hidePosition = World.Instance.GetHidingSpots()[i].transform.position + hideDir.normalized * 5;
+
+            if (Vector3.Distance(this.transform.position, hidePosition) < dist)
+            {
+                chosenSpot = hidePosition;
+                chosenDir = hideDir;
+                chosenGO = World.Instance.GetHidingSpots()[i];
+                dist = Vector3.Distance(this.transform.position, hidePosition);
+            }
+        }
+
+        Collider hideCol = chosenGO.GetComponent<Collider>();
+
+        Ray backRay = new Ray(chosenSpot, -chosenDir.normalized);
+
+        RaycastHit info;
+
+        float distance = 100.0f;
+
+        hideCol.Raycast(backRay, out info, distance);
+
+        Seek(info.point + chosenDir.normalized * 5);
+    }
+
+    bool CanSeeTarget()
+    {
+        RaycastHit raycastInfo;
+        Vector3 rayToTarget = target.transform.position - this.transform.position;
+        if(Physics.Raycast(this.transform.position, rayToTarget, out raycastInfo))
+        {
+            if (raycastInfo.transform.gameObject.tag == "cop")
+                return true;
+        }
+
+        return false;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Wander();
+        if(CanSeeTarget())
+            CleverHide();
+
     }
 }
